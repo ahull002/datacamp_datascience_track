423b0bed6e67ed6c2e7931bb3e7a0c9af2e8f199
Alfred Hull <alfred.d.hull@gmail.com>
2019-12-18 10:55:01 -0500

    Update Chapter 3 -Introduction to hypothesis testing.py
    Update Chapter 3 - Introduction to hypothesis testing.py

--- Statistical Thinking in Python (Part 2)/Chapter 3 -Introduction to hypothesis testing.py
+++ Statistical Thinking in Python (Part 2)/Chapter 3 -Introduction to hypothesis testing.py
@@ -2,10 +2,8 @@
 #Chapter 3 - Introduction to hypothesis testing
 
 
-
-
 #----------------------------------------------------------------------------------#
-#Generating a permutation sample
+__Generating a permutation sample__
  def permutation_sample(data1, data2):
         """Generate a permutation sample from two data sets."""
 
@@ -23,9 +21,7 @@
 
 
 #----------------------------------------------------------------------------------#
-#Visualizing permutation sampling
-
-
+__Visualizing permutation sampling__
 for _ in range(50):
     # Generate permutation samples
     perm_sample_1, perm_sample_2 = permutation_sample(
@@ -41,6 +37,7 @@ for _ in range(50):
     _ = plt.plot(x_2, y_2, marker='.', linestyle='none',
                  color='blue', alpha=0.02)
 
+
 # Create and plot ECDFs from original data
 x_1, y_1 = ecdf(rain_july)
 x_2, y_2 = ecdf(rain_november)
@@ -53,17 +50,16 @@ _ = plt.xlabel('monthly rainfall (mm)')
 _ = plt.ylabel('ECDF')
 plt.show()
 
-#----------------------------------------------------------------------------------#
 
-#Generating permutation replicates
+#----------------------------------------------------------------------------------#
 def draw_perm_reps(data_1, data_2, func, size=1):
     """Generate multiple permutation replicates."""
 
-    # Initialize array of replicates: perm_replicates
+    # Initialize an array to hold permutation replicates: perm_replicates
     perm_replicates = np.empty(size)
 
     for i in range(size):
-        # Generate permutation sample
+        # Compute a permutation sample using your permutation_sample() function
         perm_sample_1, perm_sample_2 = permutation_sample(data_1, data_2)
 
         # Compute the test statistic
@@ -73,8 +69,7 @@ def draw_perm_reps(data_1, data_2, func, size=1):
 
 
 #----------------------------------------------------------------------------------#
-#Look before you leap: EDA before hypothesis testing
-
+__Look before you leap: EDA before hypothesis testing__
 # Make bee swarm plot
 _ = sns.swarmplot(x='ID', y='impact_force', data=df)
 
@@ -85,10 +80,9 @@ _ = plt.ylabel('impact force (N)')
 # Show the plot
 plt.show()
 
+
 #----------------------------------------------------------------------------------#
-
-#Permutation test on frog data
-
+__Permutation test on frog data__
 def diff_of_means(data_1, data_2):
     """Difference in means of two arrays."""
 
@@ -97,39 +91,73 @@ def diff_of_means(data_1, data_2):
 
     return diff
 
+
 # Compute difference of mean impact force from experiment: empirical_diff_means
 empirical_diff_means = diff_of_means(force_a, force_b)
+
 
 # Draw 10,000 permutation replicates: perm_replicates
 perm_replicates = draw_perm_reps(force_a, force_b,
                                  diff_of_means, size=10000)
 
+
 # Compute p-value: p
 p = np.sum(perm_replicates >= empirical_diff_means) / len(perm_replicates)
+
 
 # Print the result
 print('p-value =', p)
 
+# In the above case: The p-value tells you that there is about a 0.6% chance that you would get 
+the difference of means observed in the experiment if frogs were exactly the same. A p-value below 
+0.01 is typically said to be "statistically significant," but: warning! warning! warning! You have 
+computed a p-value; it is a number. I encourage you not to distill it to a yes-or-no phrase.
+
+p = 0.006 and p = 0.000000006 are both said to be "statistically significant," but they are 
+definitely not the same!
 
 
 #----------------------------------------------------------------------------------#
-
-#A one-sample bootstrap hypothesis test
+__A one-sample bootstrap hypothesis test__
 # Make an array of translated impact forces: translated_force_b
 translated_force_b = force_b - np.mean(force_b) + 0.55
+
 
 # Take bootstrap replicates of Frog B's translated impact forces: bs_replicates
 bs_replicates = draw_bs_reps(translated_force_b, np.mean, 10000)
 
+
 # Compute fraction of replicates that are less than the observed Frog B force: p
 p = np.sum(bs_replicates <= np.mean(force_b)) / 10000
+
 
 # Print the p-value
 print('p = ', p)
 
 
 #----------------------------------------------------------------------------------#
-#A bootstrap test for identical distributions
+__A two-sample bootstrap hypothesis test for difference of means__
+# Compute mean of all forces: mean_force
+mean_force = np.mean(forces_concat)
+
+# Generate shifted arrays
+force_a_shifted = force_a - np.mean(force_a) + mean_force
+force_b_shifted = force_b - np.mean(force_b) + mean_force
+
+# Compute 10,000 bootstrap replicates from shifted arrays
+bs_replicates_a = draw_bs_reps(force_a_shifted, np.mean, size=10000)
+bs_replicates_b = draw_bs_reps(force_b_shifted, np.mean, size=10000)
+
+# Get replicates of difference of means: bs_replicates
+bs_replicates = bs_replicates_a - bs_replicates_b
+
+# Compute and print p-value: p
+p = np.sum(bs_replicates >= empirical_diff_means) / len(bs_replicates)
+print('p-value =', p)
+
+
+#----------------------------------------------------------------------------------#
+__A bootstrap test for identical distributions__
 # Compute difference of mean impact force from experiment: empirical_diff_means
 empirical_diff_means = diff_of_means(force_a, force_b)
 
@@ -153,7 +181,7 @@ print('p-value =', p)
 
 
 #----------------------------------------------------------------------------------#
-#A two-sample bootstrap hypothesis test for difference of means.
+__A two-sample bootstrap hypothesis test for difference of means.__
 # Compute mean of all forces: mean_force
 mean_force = np.mean(forces_concat)
 
@@ -173,16 +201,64 @@ p = np.sum(bs_replicates >= empirical_diff_means) / len(bs_replicates)
 print('p-value =', p)
 
 #----------------------------------------------------------------------------------#
+AB Testing: Congressional Civil Rights Act
+
+# Construct arrays of data: dems, reps
+dems = np.array([True] * 153 + [False] * 91)
+reps = np.array([True] * 136 + [False] * 35)
+
+def frac_yea_dems(dems, reps):
+    """Compute fraction of Democrat yea votes."""
+    frac = np.sum(dems) / len(dems)
+    return frac
+
+# Acquire permutation samples: perm_replicates
+perm_replicates = draw_perm_reps(dems, reps, frac_yea_dems, 10000)
+
+# Compute and print p-value: p
+p = np.sum(perm_replicates <= 153/244) / len(perm_replicates)
+print('p-value =', p)
 
 
+#----------------------------------------------------------------------------------#
+A time-on-website analog
+# Compute the observed difference in mean inter-no-hitter times: nht_diff_obs
+nht_diff_obs = diff_of_means(nht_dead, nht_live)
+
+# Acquire 10,000 permutation replicates of difference in mean no-hitter time: perm_replicates
+perm_replicates = draw_perm_reps(nht_dead, nht_live,
+                                 diff_of_means, size=10000)
+
+
+# Compute and print the p-value: p
+p = np.sum(perm_replicates <= nht_diff_obs) / len(perm_replicates)
+print('p-val =', p)
+
+#Your p-value is 0.0001, which means that only one out of your 10,000 replicates had a result as extreme 
+as the actual difference between the dead ball and live ball eras. This suggests strong statistical significance. 
+Watch out, though, you could very well have gotten zero replicates that were as extreme as the observed value. 
+This just means that the p-value is quite small, almost certainly smaller than 0.001.
+
 
 #----------------------------------------------------------------------------------#
-#----------------------------------------------------------------------------------#
-#----------------------------------------------------------------------------------#
-#----------------------------------------------------------------------------------#
+# Compute observed correlation: r_obs
+r_obs = pearson_r(illiteracy, fertility)
 
-#----------------------------------------------------------------------------------#
-#----------------------------------------------------------------------------------#
-#----------------------------------------------------------------------------------#
-#----------------------------------------------------------------------------------#
-#----------------------------------------------------------------------------------#
+# Initialize permutation replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute illiteracy measurments: illiteracy_permuted
+    illiteracy_permuted = np.random.permutation(illiteracy)
+
+    # Compute Pearson correlation
+    perm_replicates[i] = pearson_r(illiteracy_permuted, fertility)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= r_obs) / len(perm_replicates)
+print('p-val =', p)
+
+# You got a p-value of zero. In hacker statistics, this means that your p-value is very low, since you never got a single 
+replicate in the 10,000 you took that had a Pearson correlation greater than the observed one. You could try increasing 
+the number of replicates you take to continue to move the upper bound on your p-value lower and lower.
