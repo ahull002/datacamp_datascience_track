93bd8f8bfcdeadb347fabc10b971269599b9af10
Alfred Hull <alfred.d.hull@gmail.com>
2020-02-01 19:16:34 -0500

    Update Chapter 4 - Discovering interpretable features.py

--- Unsupervised Learning in Python/Chapter 4 - Discovering interpretable features.py
+++ Unsupervised Learning in Python/Chapter 4 - Discovering interpretable features.py
@@ -46,6 +46,12 @@ components_df = pd.DataFrame(model.components_, columns=words)
 # Print the shape of the DataFrame
 print(components_df.shape)
 
+# Select row 3: component
+component = components_df.iloc[3]
+
+# Print result of nlargest
+print(component.nlargest())
+
 
 #------------------------------------------------------------------------------------------------------------#
 #Explore the LED digits dataset
